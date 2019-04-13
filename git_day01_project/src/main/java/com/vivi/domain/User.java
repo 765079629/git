485930dde9868c4c6265df82c27edc7a6ab2e6b1
git-vivi <765079629@qq.com>
2019-04-13 17:03:08 +0800

    测试git的小demo

---
+++ git_day01_project/src/main/java/com/vivi/domain/User.java
@@ -0,0 +1,24 @@
+package com.vivi.domain;
+
+public class User {
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+
+
+    }
+}
---
+++ git_day01_project/src/main/java/com/vivi/domain/demo/test.java
@@ -0,0 +1,11 @@
+package com.vivi.domain.demo;
+
+import com.vivi.domain.User;
+
+public class test {
+    public static void main(String[] args) {
+        User user = new User();
+        user.setId(1);
+        user.setName("胖亮");
+    }
+}
